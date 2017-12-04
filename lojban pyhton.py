56b3008662b026340b91a5a3768cbc4c8cb534d1
Amalamol Raju <33655795+araju55@users.noreply.github.com>
2017-12-04 20:39:21 +0000

    Add files via upload

---
+++ lojban pyhton.py
@@ -0,0 +1,9 @@
+lojban = ["no", "pa", "re", "ci", "vo", "mu", "xa", "ze", "bi", "so", "no"]
+number = input("Enter your number")
+outputlojbanstringcode = ""
+For (l) in range(len(str(number))):
+    partnumber = int(number[j]))
+    outputlojbanstringcode= outputlojbanstringcode + lojban(partnumber)
+
+print(outputlojbanstringcode)
+    
